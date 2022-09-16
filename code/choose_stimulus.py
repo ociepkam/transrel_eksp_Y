8e58511e579bd9239b51e970197d3081b87c5bea
ociepkam <ociepka.m@gmail.com>
2022-09-16 13:54:51 +0200

    0.3 removed similar stimulus

--- code/choose_stimulus.py
+++ code/choose_stimulus.py
@@ -1,7 +1,10 @@
 def choose_stimulus(stimulus_type):
     if stimulus_type == "Latin":
         import string
-        stimulus = {"type": "text", "stimulus_list": list(string.ascii_uppercase)}
+        alphabet = list(string.ascii_uppercase)
+        to_remove = "IQMERQW"
+        [alphabet.remove(elem) for elem in to_remove]
+        stimulus = {"type": "text", "stimulus_list": alphabet}
     else:
         raise Exception("Unknown stimulus type")
     return stimulus["type"], stimulus["stimulus_list"]
